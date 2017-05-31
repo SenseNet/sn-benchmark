fcc8e1851137d004f6aeaf6ef8658c18e227c7ff
Zoltan Gyebrovszki <gyeby5@gmail.com>
2017-05-31 05:39:04 +0200

    Basic webserver simulation

---
+++ src/SnBenchmarkTest/LoadControl/NoiseFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SnBenchmarkTest.LoadControl
+{
+    internal class NoiseFilter
+    {
+        private readonly double _qSize;
+        private readonly double[] _buffer;
+        private int _index;
+        public double FilteredValue { get; private set; }
+
+        public NoiseFilter(int size)
+        {
+            _qSize = Convert.ToDouble(size);
+            _buffer = new double[size];
+        }
+
+        public double NextValue(double value)
+        {
+            var last = _buffer[_index];
+            _buffer[_index] = value;
+            _index = (_index + 1) % _buffer.Length;
+
+            FilteredValue += (value - last) / _qSize;
+
+            return FilteredValue;
+        }
+    }
+}
---
+++ src/SnBenchmarkTest/LoadControl/WebServerSimulatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using SNBCalc;
+
+namespace SnBenchmarkTest.LoadControl
+{
+    [TestClass]
+    public class WebServerSimulatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var server = new WebServerSimulator(40, 100);
+            var filter = new NoiseFilter(100);
+            var epc = new BenchmarkEndPointCalculator();
+            for (int profiles = 1; profiles < 60; profiles++)
+            {
+                for (int iteration = 0; iteration < 50; iteration++)
+                {
+                    var reqPerSec = server.GetRequestPerSec(profiles + 1);
+                    var filteredValue = filter.NextValue(reqPerSec);
+                    var detected = epc.Detect(reqPerSec) ? 1 : 0;
+                    Debug.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", profiles, iteration, reqPerSec, filteredValue, epc.CurrentValue*200, detected*100);
+                }
+            }
+        }
+    }
+}
