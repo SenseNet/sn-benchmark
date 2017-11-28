3483b48520831936ddf011bf05cebfb638128373
iviczl <iviczl@sensenet.com>
2017-11-28 15:20:32 +0100

    tackle assembly infos

--- src/Tools/SNBCalc/Properties/AssemblyInfo.cs
+++ src/Tools/SNBCalc/Properties/AssemblyInfo.cs
@@ -7,30 +7,12 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SNBCalc")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Sense/Net Inc.")]
+[assembly: AssemblyCopyright("Copyright © Sense/Net Inc. 2016")]
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
+[assembly: AssemblyProduct("Sense/Net Benchmark")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SNBCalc")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("81983a89-99f3-4d50-8776-0f72c414c56c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
